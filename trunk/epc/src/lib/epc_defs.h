7227785d39b79c4b7c80ccb9a360e97a048ee364
gpauliss <gpauliss@c1c61559-3a1e-0410-b1f8-a6e2dc18b5fa>
1998-02-03 10:01:48 +0000

    - Changed error codes - Added maximum string lengths - Added support for static allocation of parameters - Modified message protocol

--- trunk/epc/src/lib/epc_defs.h
+++ trunk/epc/src/lib/epc_defs.h
@@ -1,23 +1,36 @@
 #ifndef _EPC_TYPES_H_
 #define _EPC_TYPES_H_
 
-#define MAXLEN 255
-#define MAXNUMFUNS 100
-#define MAXNUMPARMS 20
+#define MAX_PIPE_NAME_LEN	255
+#define MAX_FUNC_NAME_LEN	255
+#define MAX_INTERFACE_NAME_LEN	255
+#define MAX_STR_VAL_LEN		4096
+#define MAX_FUNCTIONS		100
+#define MAX_PARAMETERS		20
+
+#include "idl_const.h"  /* constants used by idl and epc */
 
 typedef struct {
 	int mode;
 	int type;
-	void *value;
+	union {
+		int		ival;
+		double	dval;
+		float	fval;
+		char *	sval;
+	} uval;
+	void *value;	/* points to one of the union members above */
 } parameter_t;
 
 typedef struct {
-	char function_name[MAXLEN];
-	int return_type;
-	void *return_value;
+	char result_pipe[MAX_PIPE_NAME_LEN];
+	char function_name[MAX_FUNC_NAME_LEN];
+	parameter_t return_value;	/* return value is considered a parameter */
 	int num_parameters;
-	parameter_t parameters[MAXNUMPARMS];
+	parameter_t parameters[MAX_PARAMETERS];
 } call_t;
+
+#define CALL_INIT { "", "", { 0 }, 0, { { 0 } } }
 
 typedef struct {
 	char *name;
@@ -27,16 +40,26 @@ typedef struct {
 
 typedef enum {
 	OK = 0,
-	MSG_TOO_BIG = -1,
-	MSG_INTERRUPTED = -2,
-	FUNCTION_UNKNOWN = -3,
-	PARAMETER_UNKNOWN = -4,
-	EMPTY_PIPE = -5,
-	NO_RESULT_PIPE = -6,
-	NO_FUNCTION = -7,
-	NO_PARAMETER = -8,
-	RECEIVE_ERROR = -9,
-	EXEC_ERROR = -10
+
+		/* dbms_pipe.send_message/dbms_pipe.receive_message status */
+	MSG_TIMED_OUT = -1,
+	MSG_TOO_BIG = -2,
+	MSG_INTERRUPTED = -3,
+
+		/* miscellaneous send/receive errors */
+	RECEIVE_ERROR = -4,
+	SEND_ERROR = -5,
+
+		/* error during execution of function requested */
+	EXEC_ERROR = -6,
+
+		/* could not allocate a value */
+	MEMORY_ERROR = -7,
+
+		/* illegal values in message */
+	DATATYPE_UNKNOWN = -8,
+	PARAMETER_MODE_UNKNOWN = -9,
+	FUNCTION_UNKNOWN = -10
 } error_t;
 
 #endif
