50fd0d9d4a064a21f4492b5ff7ba362bd72f759f
gjp <gjp@c1c61559-3a1e-0410-b1f8-a6e2dc18b5fa>
1998-08-19 06:35:49 +0000

    Added epc_handle_request.

--- trunk/epc/src/lib/epc.h
+++ trunk/epc/src/lib/epc.h
@@ -15,6 +15,8 @@ extern epc_error_t epc_set_pipe( epc_info_t *epc_info, char *pipe );
 
 extern epc_error_t epc_add_interface( epc_info_t *epc_info, epc_interface_t *interface );
 
+extern long epc_handle_request( epc_info_t *epc_info, epc_call_t *call );
+
 extern epc_error_t epc_handle_requests( epc_info_t *epc_info );
  
 #endif
