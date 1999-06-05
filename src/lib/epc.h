aa6aaf912a1cf44948e702a12e0f56580d619571
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
1999-06-05 12:18:33 +0000

    Renamed my_ functions to ts_

--- src/lib/epc.h
+++ src/lib/epc.h
@@ -5,17 +5,18 @@
 
 /*
 || Because Oracle uses epc_ functions as well on Linux (epc_init),
-|| I will rename my EPC functions by using my_epc.
+|| I will rename the EPC functions to ts_epc (Transfer Solutions)
+|| Be sure that in ts_epc-def.cpp the ts_ names are used.
 */
 
-#define epc_add_interface my_epc_add_interface
-#define epc_done my_epc_done
-#define epc_handle_request my_epc_handle_request
-#define epc_handle_requests my_epc_handle_requests
-#define epc_init my_epc_init
-#define epc_main my_epc_main
-#define epc_set_logon my_epc_set_logon
-#define epc_set_pipe my_epc_set_pipe
+#define epc_add_interface ts_epc_add_interface
+#define epc_done ts_epc_done
+#define epc_handle_request ts_epc_handle_request
+#define epc_handle_requests ts_epc_handle_requests
+#define epc_init ts_epc_init
+#define epc_main ts_epc_main
+#define epc_set_logon ts_epc_set_logon
+#define epc_set_pipe ts_epc_set_pipe
 
 extern epc_error_t epc_main( int argc, char **argv, epc_interface_t * );
 
