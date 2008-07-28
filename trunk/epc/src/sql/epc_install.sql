709b018c32b4882bf2afa034b24bbe759b7954b3
gpaulissen <gpaulissen@c1c61559-3a1e-0410-b1f8-a6e2dc18b5fa>
2008-07-28 12:04:38 +0000

    Updated install.sql

--- trunk/epc/src/sql/epc_install.sql
+++ trunk/epc/src/sql/epc_install.sql
@@ -3,16 +3,29 @@ REMARK
 REMARK  $HeadURL$
 REMARK
 
+prompt @@std_object.typ
 @@std_object.typ
+prompt @@epc_clnt_object.typ
 @@epc_clnt_object.typ
+prompt @@std_objects.tab
 @@std_objects.tab
+prompt @@std_object_mgr.pks
 @@std_object_mgr.pks
+prompt @@epc.pks
 @@epc.pks
+prompt @@epc_srvr.pks
 @@epc_srvr.pks
+prompt @@epc_clnt.pks
 @@epc_clnt.pks
+prompt @@std_object.tyb
 @@std_object.tyb
+prompt @@epc_clnt_object.tyb
 @@epc_clnt_object.tyb
+prompt @@std_object_mgr.pkb
 @@std_object_mgr.pkb
+prompt @@epc.pkb
 @@epc.pkb
+prompt @@epc_srvr.pkb
 @@epc_srvr.pkb
+prompt @@epc_clnt.pkb
 @@epc_clnt.pkb
